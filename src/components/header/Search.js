75248aa9b1bf3f8e2d8cdecd7147c8875e81138b
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-08-21 20:36:53 +0200

    "

--- src/components/header/Search.js
+++ src/components/header/Search.js
@@ -6,7 +6,7 @@ import {useLocation } from "react-router";
 
 export default function Search() {
   const location = useLocation()
-  const searchValue = location.state.searchValue
+  const searchValue = location.state.searchValue ? location.state.searchValue :  " "
   const url =  searchValue.split(" ").slice(0, 4).join("%20")
   const [content, setContent] = React.useState([]);
 
@@ -21,6 +21,7 @@ export default function Search() {
       <h2 className="text-2xl font-bold mb-2 text-gray-800"> Your search</h2>
       <div className="container  my-12 mx-auto px-4 md:px-12">
         <div className="flex flex-wrap items-center justify-around -mx-1 lg:-mx-4">
+         
           {content.map((elem, index) => {
             return (
               <Card
