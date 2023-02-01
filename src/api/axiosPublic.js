7dfc3fbaed23d82302d02142a8a739366a018bec
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2023-02-01 20:48:06 +0200

    NEW York Review api

--- src/api/axiosPublic.js
+++ src/api/axiosPublic.js
@@ -9,7 +9,18 @@ export const axiosPublic = axios.create({
   },
 });
 
+//NYbook api 
+
 export const getBookPage = async (pageParam = 1) => {
   const response = await axiosPublic.get(`/?page=${pageParam}`)
   return response
 }
+
+
+export const axiosNY = axios.create({
+  baseURL: "https://api.nytimes.com/svc/books/v3/reviews.json?author=Haruki%20Murakami&api-key=C2ML18ksOuaQDwXbCu3IyYEeAOvp4d2x",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+// axiosNY.defaults.headers['X-API-KEY'] = 'C2ML18ksOuaQDwXbCu3IyYEeAOvp4d2x';
